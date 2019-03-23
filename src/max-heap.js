0a4d6a57a7273d1ffced6f98b2da5d70aab5221f
RomanDyba <romansmaga@mailfence.com>
2019-03-23 21:28:42 +0300

    feat: task complete

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,138 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.number = 0;
 	}
 
 	push(data, priority) {
-		
+		const node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (this.root) {
+			const r = this.root.data;
+			if (this.parentNodes.length > 0)
+				 this.number--;
+
+			this.restoreRootFromLastInsertedNode(this.detachRoot());
+			this.shiftNodeDown(this.root);
+			
+			return r;
+		}
 	}
 
 	detachRoot() {
-		
+		const a = this.root;
+		this.root = null;
+		if (this.parentNodes[0] === a) 
+			this.parentNodes.shift();
+		return a;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		if (this.parentNodes.length != 0) {
+			const n = this.parentNodes[this.parentNodes.length-1];
+
+			if (this.parentNodes.length == 2) {
+				const b = this.parentNodes.pop();
+				this.parentNodes.unshift(b);
+			}	
+			else if (this.parentNodes.length > 2) 
+				if (n.parent.right === n) {
+					this.parentNodes.unshift(n.parent);
+					this.parentNodes.pop();
+				}
+				else this.parentNodes.pop();
+
+			n.remove();
+			this.root = n;
+			if (detached.left){ 
+				n.left = detached.left;
+				detached.left.parent = n;
+			}
+			if (detached.right) {
+				n.right = detached.right;
+				detached.right.parent = n;
+			}
+		}
 	}
 
 	size() {
-		
+		return this.number;
 	}
 
 	isEmpty() {
-		
+			return !this.size(); 
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.number = 0;
 	}
 
 	insertNode(node) {
-		
+		if (!this.root)
+			this.root = node;
+		else if (!this.parentNodes[0].left) {
+				this.parentNodes[0].appendChild(node);
+			}
+		else if (!this.parentNodes[0].right) {
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.shift();
+		}
+		this.parentNodes.push(node);
+		this.number++;
 	}
-
+	
 	shiftNodeUp(node) {
-		
+		if (node.parent) {
+			if (node.priority > node.parent.priority) {
+				const n = this.parentNodes.indexOf(node);
+				if (this.parentNodes.indexOf(node.parent) > -1)
+					this.parentNodes[this.parentNodes.indexOf(node.parent)] = node;
+				this.parentNodes[n] = node.parent;
+				
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			}
+		}
+		else {
+			this.root = node;
+		}
 	}
 
 	shiftNodeDown(node) {
-		
+		if (node && node.left) 
+			if (node.right && Math.max(node.priority, node.left.priority, node.right.priority) == node.right.priority) {
+				if (this.parentNodes.indexOf(node.right) > -1)
+					this.parentNodes[this.parentNodes.indexOf(node.right)] = node;
+				
+				if (this.root === node) 
+					this.root = node.right;
+
+				node.right.swapWithParent();
+				this.shiftNodeDown(node);
+			}
+			else if ((!node.right && node.left.priority > node.priority) || (node.right && Math.max(node.priority, node.left.priority, node.right.priority) == node.left.priority))	{
+				if (this.parentNodes.indexOf(node) > -1) {
+					const n = this.parentNodes.indexOf(node);
+					this.parentNodes[this.parentNodes.indexOf(node.left)] = node;
+					this.parentNodes[n] = node.left;
+				}
+				else if (this.parentNodes.indexOf(node.left) > -1)
+					this.parentNodes[this.parentNodes.indexOf(node.left)] = node;
+			
+				if (this.root === node)
+					this.root = node.left;
+
+				node.left.swapWithParent();
+				this.shiftNodeDown(node);
+			}
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,22 +1,83 @@
 class Node {
 	constructor(data, priority) {
-
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
 	}
 
 	appendChild(node) {
-
+    if (!this.left) {
+      this.left = node;
+      node.parent = this;
+    }
+    else if (!this.right) {
+      this.right = node;
+      node.parent = this;
+    } 
 	}
 
 	removeChild(node) {
-
+    if (node === this.left) {
+      this.left = null;
+      node.parent = null;
+    }
+    else if (node === this.right) {
+      this.right = null;
+      node.parent = null;
+    }
+    else throw new Error("Node has no children");
 	}
 
 	remove() {
-
+    if (this.parent) 
+      this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+    if (this.parent) {
+      const temp_parent = this.parent;
+       
+      if (this.parent.left === this)
+        var side = 1;
+      else side = 2;
+      this.remove();
+      
+      if (this.left) {
+        var temp_left = this.left;
+        this.left.remove();
+      }
+
+      if (this.right) {
+        var temp_right = this.right;
+        this.right.remove();
+      }
+
+      if (temp_parent.parent) {
+        var temp = temp_parent.parent;
+        temp_parent.remove();
+        temp.appendChild(this);
+      }
+
+    if (side == 1) {
+      this.appendChild(temp_parent);
+      if (temp_parent.right){
+        temp = temp_parent.right;
+        temp_parent.right.remove();
+        this.appendChild(temp);
+      }
+    }
+    else {
+      temp = temp_parent.left;
+      temp_parent.left.remove();
+      this.appendChild(temp);
+      this.appendChild(temp_parent);
+    }
+
+      if (temp_left) temp_parent.appendChild(temp_left);
+      if (temp_right) temp_parent.appendChild(temp_right);
+    }
 	}
 }
 
